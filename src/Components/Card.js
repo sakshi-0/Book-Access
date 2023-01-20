2924d65403ed37144fcc5d038270bc986bd1c952
sakshi-0 <116291283+sakshi-0@users.noreply.github.com>
2023-01-20 21:05:12 +0530

    Add files via upload

---
+++ src/Components/Card.js
@@ -0,0 +1,37 @@
+import react from "react";
+import { useState } from "react";
+import Modal from "./Modal";
+const Card = ({ book }) => {
+
+    const [show,setShow]=useState(false);
+    const [bookItem,setItem]=useState();
+    console.log(book)
+    return (
+        <>
+            {
+                book.map((item) => {
+                    let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+                    let Price=item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+                    if(thumbnail!= undefined && Price!=undefined)
+                    {
+                        return (
+                            <>
+                            <div className="card" onClick={()=>{setShow(true);setItem(item)}}>
+                                <img src={thumbnail} alt="" />
+                                <div className="bottom">
+                                    <h3 className="title">{item.volumeInfo.title}</h3>
+                                    <p className="Price">&#8377;{Price}</p>
+                                </div>
+                            </div>
+                              <Modal show={show} item={bookItem} onClose={()=>setShow(false)}/>
+                            </>
+                        )
+                    }
+
+                })
+            }
+
+        </>
+    )
+}
+export default Card;
